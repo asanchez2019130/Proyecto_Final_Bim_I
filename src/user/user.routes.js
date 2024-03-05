8e5de243158dca3269512fe7a4c52f18472b7b5b
asanchez2019130 <asanchez-2019130@kinal.edu.gt>
2024-03-04 18:16:14 -0600

    Se realizo el CRUD base de User

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -3,47 +3,36 @@ import { check } from "express-validator";
 import {
   getUsers,
   createUser,
-  getUserById,
   updateUser,
   deleteUser,
 } from "./user.controller.js";
 import {
   existenteEmail,
-  esRoleValido,
+  //esRoleValido,
   existeUsuarioById,
 } from "../helpers/db-validators.js";
 import { validarCampos } from "../middlewares/validar-campos.js";
-import { tieneRole } from "../middlewares/validar-roles.js";
-import { validarJWT } from "../middlewares/validar-jwt.js";
+//import { tieneRole } from "../middlewares/validar-roles.js";
+//import { validarJWT } from "../middlewares/validar-jwt.js";
 
 const router = Router();
 
 // Ruta para obtener la lista de usuarios
 router.get("/", getUsers);
 
-// Ruta para obtener un usuario por su ID
-router.get(
-  "/:id",
-  [
-    check("id", "No es un ID válido").isMongoId(),
-    check("id").custom(existeUsuarioById),
-    validarCampos,
-  ],
-  getUserById
-);
-
 // Ruta para crear un nuevo usuario
 router.post(
   "/",
   [
-    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("firstName", "El nombre es obligatorio").not().isEmpty(),
+    check("lastName", "El nombre es obligatorio").not().isEmpty(),
+    check("userName", "El nombre es obligatorio").not().isEmpty(),
     check("password", "El password debe ser mayor a 6 caracteres").isLength({
       min: 6,
     }),
-    check("correo", "Este no es un correo válido").isEmail(),
-    check("correo").custom(existenteEmail),
-    check("role").custom(esRoleValido),
-    check("phone","El teléfono debe de contener 8 números").isLength({min: 8, max:8}),
+    check("email", "Este no es un correo válido").isEmail(),
+    check("email").custom(existenteEmail),
+    //check("role").custom(esRoleValido),
     validarCampos,
   ],
   createUser
@@ -64,8 +53,8 @@ router.put(
 router.delete(
   "/:id",
   [
-    validarJWT,
-    tieneRole("ADMIN_ROLE", "VENTAS_ROLE"),
+    //validarJWT,
+    //tieneRole("ADMIN_ROLE", "VENTAS_ROLE"),
     check("id", "No es un ID válido").isMongoId(),
     check("id").custom(existeUsuarioById),
     validarCampos,
